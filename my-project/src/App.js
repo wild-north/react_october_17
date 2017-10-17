107c8b97cf6f3d73a37d986415fa8bba4192acb4
Serhii Strizhak <Serhii_Strizhak@epam.com>
2017-10-17 18:03:16 +0300

    lesson_03: add react router examples

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,112 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 
-class TodoListApp extends Component {
-  constructor(args) {
-    super(args);
-    this.state = {
-      list: []
-    };
-    this.addNewTask = this.addNewTask.bind(this);
-    this.removeTask = this.removeTask.bind(this);
-  }
-  addNewTask(text) {
-    this.setState({
-      list: [...this.state.list, text]
-    })
-  }
-  removeTask(id) {
-    const copy = this.state.list.slice();
-    copy.splice(id, 1);
-    this.setState({
-      list: copy
-    })
-  }
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const ProductButton = ({ history }) => (
+    <button onClick={ () => history.push('/product') }>Products</button>
+);
 
-
-  render() {
-  const { list } = this.state;
-
+const ProductLink = withRouter(ProductButton);
+const LinksSection = () => (
+    <nav>
+        <Link to="/">Home</Link>
+        {' |||| '}
+        <Link to={ {pathname: '/about'} }>About</Link>
+        {' |||| '}
+        <ProductLink />
+    </nav>
+);
+const ProductList = (props) => {
+    const products = [
+        {id: 100, name: 'товар 1', desc: 'охренительная штука всего за $99.99'},
+        {id: 101, name: 'товар 2', desc: 'еще болле охренительная штука всего за $999.99'},
+        {id: 102, name: 'товар 3', desc: 'мегаохренительная штука всего за $1099.99'},
+    ];
     return (
-      <div>
-        <Form addNewTask={ this.addNewTask }/>
-        <FormWithRefs addNewTask={ this.addNewTask }/>
-        <List list={ list } onRemove={ this.removeTask }/>
-      </div>
+        <div>
+            <h1>Product List:</h1>
+            <ul>
+                {
+                    products.map(({ id, name, desc }) => (
+                        <li key={ id }>
+                            <a href="" onClick={ (e) => e.preventDefault() || props.history.push(`/product/${id}`) }><strong>{ name }</strong></a>
+                            <div>{ desc }</div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
-  }
-}
+};
 
-class Form extends Component{
-  constructor(args) {
-    super(args);
-    this.state = {
-      text: 'Привет, я - форма.'
-    };
+const ProductDetails = ({ productId }) => <h1>Product #{ productId } Details</h1>;
 
-    this.inputChange = this.inputChange.bind(this);
-    this.formSubmit = this.formSubmit.bind(this);
-  }
+const App = () => (
+    <Router>
+        <div>
+            <LinksSection />
+            <Route exact path="/" component={ Home }/>
+            <Route path="/about" component={ About }/>
+            <Route exact path="/product" render={ (props) => <ProductList { ...props }/> }/>
+            <Route path="/product/:productId" render={ ({ match }) => <ProductDetails { ...match.params } /> }/>
+            {/*<Route path="/about" children={ () => <About /> }/>*/}
+        </div>
+    </Router>
+);
 
-  inputChange(ev) {
-    this.setState({
-      text: ev.target.value
-    })
-  }
-  formSubmit(ev) {
-    ev.preventDefault();
-    this.setState({
-      text: ''
-    })
-    this.props.addNewTask(this.state.text);
-  }
-
-  render() {
-    return (
-      <form onSubmit={ this.formSubmit }>
-        <input type="text"
-          value={ this.state.text }
-          onChange={ this.inputChange }
-          />
-      </form>
-    );
-  }
-}
-
-class FormWithRefs extends Component{
-  constructor(args) {
-    super(args);
-
-    this.formSubmit = this.formSubmit.bind(this);
-  }
-
-  formSubmit(ev) {
-    ev.preventDefault();
-
-    this.props.addNewTask(this.myInput.value);
-    this.myInput.value = '';
-  }
-
-  render() {
-    return (
-      <form onSubmit={ this.formSubmit }>
-        <input type="text" ref={ (element) => {
-          this.myInput = element;
-        } }/>
-      </form>
-    );
-  }
-}
-
-const List = ({ list, onRemove }) => (
-  <ul>
-    {
-      list.map((item, index) => (
-        <li key={ index } onClick={ () => onRemove(index) }>
-          { item }
-        </li>
-      ))
-    }
-  </ul>
-)
-
-export default TodoListApp;
+export default App;
